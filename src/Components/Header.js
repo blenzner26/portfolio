4929dcc32db356fb93f27c30f890ca2ecabf37b1
Brendan Lenzner <blenzner2614@gmail.com>
2020-07-23 19:52:17 -0500

    logo addition

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,13 +2,20 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+import logo from '../imgs/bl_logo_comb.png';
 
+const styles = {
+  logo: {
+      width: "40px",
+      marginRight: "10px",
+  }
+}
 function Header(props) {
   return (
     <div className="header">
       <div className="navigation">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand as={Link} to="/">Brendan Lenzner</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/"><img style={styles.logo} src={logo} alt="Brendan Lenzner logo" /> Brendan Lenzner</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
