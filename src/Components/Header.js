11b9ba4a27a360bd454618b72fd633e736fc97f6
Brendan Lenzner <blenzner2614@gmail.com>
2021-04-20 22:13:14 -0500

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,30 +6,28 @@ import logo from '../imgs/bl_logo_comb.png';
 
 const styles = {
   logo: {
-      width: "40px",
-      marginRight: "10px",
+    width: "40px",
+    marginRight: "10px",
   }
 };
 
-function Header(props) {
-
+function Header() {
   return (
     <div className="header">
-      <div className="navigation">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand as={Link} to="/"><img style={styles.logo} src={logo} alt="Brendan Lenzner logo" /> Brendan Lenzner</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
-              <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
-              <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/"><img style={styles.logo} src={logo} alt="Brendan Lenzner logo" /> Brendan Lenzner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
+            <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
+            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
     </div>
   )
 }
